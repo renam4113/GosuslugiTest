a11b1c5fb77a0706ba68bf2ea070b15299a36ad8
renamshina <113158754+renam4113@users.noreply.github.com>
2024-02-12 20:07:47 +0900

    Add files via upload

---
+++ UnitTest.cs
@@ -0,0 +1,45 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using SeleniumExtras.WaitHelpers;
+using OpenQA.Selenium.Support.UI;
+
+namespace GosuslugiTest
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+        private readonly By _SignButton = By.XPath("//button[text() = 'Войти']");
+        private readonly By _CheckButton = By.XPath("//button[contains(text(), 'Не удаётся войти?')]");
+        private readonly By _passwordRecoveryButton = By.XPath("//button[contains(text(), 'восстановления пароля')]");
+        private const string _ExpectedUrl = "https://esia.gosuslugi.ru/login/recovery";
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.gosuslugi.ru");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void Test()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+               
+                wait.Until(ExpectedConditions.ElementToBeClickable(_SignButton)).Click();
+                wait.Until(ExpectedConditions.ElementToBeClickable(_CheckButton)).Click();
+                wait.Until(ExpectedConditions.ElementToBeClickable(_passwordRecoveryButton)).Click();   
+                wait.Until(driver => driver.Url.Contains(_ExpectedUrl));
+            
+            Assert.IsTrue(string.Compare(_ExpectedUrl, driver.Url, StringComparison.OrdinalIgnoreCase) == 0, "The password recovery is not open");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+    }
+}
